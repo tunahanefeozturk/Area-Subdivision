208ce6321345df828ff40de850811897320daccb
tunahanefeozturk <ozturktunahanefe@gmail.com>
2024-06-29 15:01:18 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,7 +2,26 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#define BIT_SET(VAR,BIT_NO)do{VAR|=1<<BIT_NO;}while(0)
+#define BIT_RESET(VAR,BIT_NO)do{VAR&=~(1<<BIT_NO);}while(0)
+#define BIT_TOGGLE(VAR,BIT_NO)do{VAR^=1<<BIT_NO;}while(0)
+#define BIT_GET(VAR,BIT_NO) ((VAR>>BIT_NO)& 1)
 
+void print_bit(int var,int bit_count){
+    int i;
+    for(i=bit_count-1;i>=0;i--){
+        printf("%d",BIT_GET(var,i));
+    }
+    printf("\n");
+}
+int main(){
+    int a=0b0011;
+    print_bit(a,4);
+}
+
+
+
+/*
 // Çizilen karenin komşu olmaması gereken bir kare varsa o kareyi çizdirmez
 bool komsu_kontrol(int x,int y,int m ){
 
@@ -355,4 +374,4 @@ int main() {
 
     return 0;
 }
-
+*/
